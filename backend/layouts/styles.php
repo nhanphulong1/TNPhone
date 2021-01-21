ae1608caa4fa3f4b0ea5be3e48bbf302ae5ef7fa
nhanphulong1 <nhanb1706619@student.ctu.edu.vn>
2021-01-21 16:17:29 +0700

    sửa test

--- backend/layouts/styles.php
+++ backend/layouts/styles.php
@@ -1,7 +1,6 @@
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="/TNPhone/assets/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
 <!-- Font awesome -->
-<link rel="styleshee1" href="/TNPhone/assets/vendor/font-awesome/css/font-awesome.min.css" type="text/css" />
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="/TNPhone/assets/vendor/font-awesome/css/font-awesome.min.css" type="text/css" />
 <!-- Custom css - Các file css do chúng ta tự viết -->
 <!-- <link rel="stylesheet" href="/TNPhone/assets/backend/css/style.css" type="text/css" /> -->
