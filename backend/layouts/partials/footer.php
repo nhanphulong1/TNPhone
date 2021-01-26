0003dd8a1a4f2dff978f9dd4243910d6b0ef0252
Nguyễn Hùng Thuận <74907065+nhthuand20100@users.noreply.github.com>
2021-01-26 16:20:42 +0700

    Chỉnh sửa giao diện header & footer

--- backend/layouts/partials/footer.php
+++ backend/layouts/partials/footer.php
@@ -3,9 +3,15 @@
         <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-10">
-                <span>Bản quyền &copy; bởi <a href="/TNPhone/index.php">TNPhone</a> - <script>document.write(new Date().getFullYear());</script>.</span>
-                <span class="text-muted">Make up your style</span>
+                <span>Bản quyền &copy; bởi <a href="/TNPhone/index.php">TNPhone</a> - <script>
+                        document.write(new Date().getFullYear());
+                    </script>-</span>
+                <span> Make up your style</span>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-10"><span>Designed by Nguyen Hung Thuan - D20221 & Dao Van Nhan - D20223</span></div>
         </div>
     </div>
 </footer>
--- backend/layouts/partials/header.php
+++ backend/layouts/partials/header.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-dark bg-dark shadow">
-  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/"><img src="/TNPhone/shared/logo.png" width="40px" style="border-radius:5px;"> TNPhone </a>
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/"><img src="/TNPhone/shared/logo.png" width="20px" style="border-radius:5px;"> TNPhone </a>
   <ul class="navbar-nav px-3 mr-auto">
     <li class="nav-item text-nowrap">
-      <a class="nav-link" href="/backend/pages/dashboard.php">Bảng tin</a>
+      <a class="nav-link" href="/backend/pages/dashboard.php">Admin Page</a>
     </li>
   </ul>
 </nav>
